d8413ff3b148d99d914e372c5861b014aeeea7af
Ileana Ordonez <ileana.s.ordonez@gmail.com>
2017-11-16 11:18:31 -0500

    created components

--- movies-app/client/src/App.js
+++ movies-app/client/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/home';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      user: null,
+      auth: false,
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="container">
+          <Route exact path='/' component={Home} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
---
+++ movies-app/client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+  return(
+    <div className="footer">
+      <p>this is the footer section</p>
+    </div>
+  )
+}
+
+export defautl Footer;
